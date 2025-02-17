8481e2876280cda7498b1b46900253cb80c91294
artur <arturminchukov@yandex.ru>
2025-02-17 16:33:25 +0300

    Added eslint rule

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-
+import stylisticJs from '@stylistic/eslint-plugin-js'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -9,4 +9,14 @@ export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  ...{
+    plugins: {
+      '@stylistic/js': stylisticJs
+    },
+    rules: {
+      "@stylistic/js/object-curly-spacing": ["error", "never"],
+      "@stylistic/js/quotes": ["error", "double"],
+      "@stylistic/js/indent": ["error", 4],
+    }
+  }
 ];
