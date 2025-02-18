bcfd12e9bf4886548bb8e3c442947237aed31825
artur <arturminchukov@yandex.ru>
2025-02-18 16:11:25 +0300

    Added alias for main directories in src

--- eslint.config.js
+++ eslint.config.js
@@ -3,20 +3,22 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import stylisticJs from '@stylistic/eslint-plugin-js'
 
+const customConfig = {
+  plugins: {
+    '@stylistic/js': stylisticJs
+  },
+  rules: {
+    "@stylistic/js/object-curly-spacing": ["error", "never"],
+    "@stylistic/js/quotes": ["error", "double"],
+    "@stylistic/js/indent": ["error", 4],
+  }
+}
+
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,ts}"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  ...{
-    plugins: {
-      '@stylistic/js': stylisticJs
-    },
-    rules: {
-      "@stylistic/js/object-curly-spacing": ["error", "never"],
-      "@stylistic/js/quotes": ["error", "double"],
-      "@stylistic/js/indent": ["error", 4],
-    }
-  }
+  ...customConfig
 ];
